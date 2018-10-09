b8fdb2e1edd09d0ee5296ba70a1590b40d4f6d33
Taji Abdullah <tabdullah@wellframe.com>
2018-10-09 14:18:34 -0400

    Added react component for a jumbotron.

--- src/component/App.js
+++ src/component/App.js
@@ -2,15 +2,14 @@ import React, {Component} from 'react';
 import '../css/App.css';
 import Header from './Header';
 import Footer from "./Footer";
+import Jumbotron from "./Jumbotron";
 
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <Header/>
-                <p className="App-intro">
-                    Will this ever become anything, only time will tell...
-                </p>
+                <Jumbotron/>
                 <Footer/>
             </div>
         );
---
+++ src/component/Jumbotron.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Jumbotron() {
+    return (
+        <div className="jumbotron">
+            <div className="container">
+                <p>yeah yo</p>
+            </div>
+        </div>
+    );
+}
+
+export default Jumbotron;
