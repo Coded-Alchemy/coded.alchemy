32bb82a7f37e5efbeb1a6937f23fb4712e619d38
Taji Abdullah <tabdullah@wellframe.com>
2018-10-12 13:47:26 -0400

    Revert

--- src/component/Jumbotron.js
+++ src/component/Jumbotron.js
@@ -1,13 +1,48 @@
-import React from "react";
+import React, {Component} from "react";
+import {classify} from "../util/Utils";
+import Container from "./Container";
 
-function Jumbotron() {
-    return (
-        <div className="jumbotron">
-            <div className="container">
-                <p>yeah yo</p>
+export default class Jumbotron extends Component {
+
+    componentWillMount() {
+        this.state = {
+            className: this.props.className || ''
+        }
+    }
+
+    render() {
+        const PROPS = Object.assign({}, this.props);
+        const CSS_NAME = 'jumbotron';
+        PROPS.className = classify(CSS_NAME, this.state.className);
+        const containerFluid = PROPS.containerFluid;
+        delete PROPS.containerFluid;
+        const body = PROPS.className.includes(CSS_NAME + '-fluid') ?
+            <Container fluid={containerFluid}>
+                {PROPS.children}
+            </Container> : PROPS.children;
+
+        return (
+            <div {...PROPS} onClick={() => {
+                this.setState({
+                    className: 'matt has much class',
+                    awesomeProperty: 12
+                })
+            }}>
+                <Container fluid={containerFluid}>
+                    {body}
+                </Container>
+
+                {/*<p>Taji was here!</p>*/}
+                {/*<Button className="-primary" href="https://google.com" target="_blank"/>*/}
             </div>
-        </div>
-    );
-}
+        )
+    }
 
-export default Jumbotron;
+    // componentDidMount(){
+
+    //     $.ajax('http://data.com').then((res)=>{res.json()})
+    //     api.fetchData().then((data)=>{
+    //        this.setState({serverData:data});
+    //     });
+    // }
+}
